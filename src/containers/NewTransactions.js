e302968765caf36860c29bd85d055afac3ce38a3
Egor Zhelyak <zhelyakea@gmail.com>
2017-10-16 18:52:44 +0300

    fixes

--- src/containers/NewTransactions.js
+++ src/containers/NewTransactions.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 
 import { getBanks } from "../actions/BanksActions";
 import { addTransaction } from "../actions/NewTrnsActions";
--- src/containers/Transactions.js
+++ src/containers/Transactions.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 
 import { deleteTransaction } from "../actions/TrnsActions";
 import { getTransactionsData } from "../actions/NewTrnsActions";
